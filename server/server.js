f8e2b7b1fe12461cd6a48ef0bad387df1944b90f
Debashish Chowdhury <rahul062002@gmail.com>
2025-12-19 11:43:43 +0600

    Add static file serving for uploads folder to display profile images

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Serve uploaded files statically
+app.use('/uploads', express.static('uploads'));
+
 connectDB();
 
 app.use("/api/auth", authRoutes);
